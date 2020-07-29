485b8d545d88d99350bf83b54c62f348b7045cfe
fujikawa <kyuji123456@yahoo.co.jp>
2020-07-29 23:54:16 +0900

    スイッパーの実装

---
+++ assets/js/swiper.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper ('.swiper-container', {
+	loop: true,
+	slidesPerView: 2,
+  	spaceBetween: 10,
+  	centeredSlides : true,
+	pagination: '.swiper-pagination',
+	nextButton: '.swiper-button-next',
+	prevButton: '.swiper-button-prev'
+}) 
