2572e7191fc400356ea827320a3588d81975ed47
fujikawa <kyuji123456@yahoo.co.jp>
2020-08-11 22:57:01 +0900

    wp載せた

---
+++ assets/js/min.js
@@ -0,0 +1,29 @@
+'use strict';
+$(function(){
+  // #で始まるリンクをクリックしたら実行されます
+  $('a[href^="#"]').click(function () {
+    // スクロールの速度
+    var speed = 500; // ミリ秒で記述
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
+});
+
+
+$(document).ready(function() {
+    var pagetop = $('.btn-back-to-top'); //class="btn-back-to-top" の部分をクリックしたとき、
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+    pagetop.fadeIn();
+      } else {
+    pagetop.fadeOut();
+      }
+    });
+    pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500); //500コマ=0.5秒かけてトップへ戻る。
+      return false;
+    });
+  });
