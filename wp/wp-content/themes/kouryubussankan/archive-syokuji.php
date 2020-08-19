24cc16cea8a3cb8c5432b1213b85cf8e329d4e08
fujikawa <kyuji123456@yahoo.co.jp>
2020-08-20 00:09:36 +0900

    画像の差し替え、追加

---
+++ wp/wp-content/themes/kouryubussankan/archive-syokuji.php
@@ -0,0 +1,559 @@
+<?php get_header(); ?>
+<div class="main-visual syokuji">
+        <h2><img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji-logo.png" alt="フレッシュベジフルおおた"></h2>
+        <div class="sub-visual">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji-top.png" alt="">
+        </div>
+</div>
+
+<section class="lead syokuji">
+    <div class="inner">
+        <p class="main-visual__description font-size--LL fw--bold">旬の味、自慢の<span class="col-yellow">まぐろ。</span><br>食材は<span class="col-yellow">新鮮</span>なものしか扱わず、<span class="col-yellow">旬のお料理</span>を楽しめるこだわりのお店です。<br><span class="col-yellow">絶品の味わい</span>をご賞味ください。</p>
+        <figure>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_lead_img.jpg" alt="" class="leadImage">
+            <figcaption>
+                <p class="text font-size--L col-yellow">まぐろの目利き職人が厳選した極上のまぐろ専門店ならではの新鮮なまぐろをご堪能ください。</p>
+                <p class="text font-size--M">内陸県で味わう、厳選された当店自慢の本格まぐろ。お料理は旬の食材にこだわり、メイン料理からデザートまですべて手作りでご提供します。一度食べたら舌の記憶に残る上品な味を、ぜひ『海鮮 加一』で。 </p>
+            </figcaption>
+        </figure>
+    </div>
+</section>
+<section class="menu syokuji"> <!--お品書き -->
+    <div class="title"> 
+    <h3><img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_title_01_text.png" alt="おすすめ商品" class="ttl_text"></h3>
+    </div>
+    <div class="inner">
+        <div class="flex pt50 pb50">
+        <div class="table__box font-size--M">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_img01.jpg" alt="">
+            <div class="table__box-text">
+            <p class="table__box-title">丼ぶり</p>
+            <table>
+                <tbody>
+                <tr>
+                <td>
+                <p class="flex"><span>加一丼</span>
+                <span>1,550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろ上トロ丼</span>
+                <span>1,850円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろ中トロ丼</span>
+                <span>1,550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ミックス丼</span>
+                <span>1,150円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろとブリのてりやき丼</span>
+                <span>1,200円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろ赤身丼</span>
+                <span>980円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ネギトロ丼</span>
+                <span>1,100円</span></p>
+                </td>
+                </tr>
+                </tbody>
+            </table>
+            </div>
+        </div>
+
+        <div class="table__box font-size--M">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_img02.jpg" alt="">
+            <div class="table__box-text">
+            <p class="table__box-title">定食</p>
+            <table>
+                <tbody>
+                <tr>
+                <td>
+                <p class="flex"><span>自慢のまぐろカツセット</span>
+                <span>850円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>旬の刺身の盛合セット</span>
+                <span>1,550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろ赤身刺身セット</span>
+                <span>980円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろ上トロ刺身セット</span>
+                <span>1,850円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろ中トロ刺身セット</span>
+                <span>1,550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ブリとサーモンの和風ステーキセット</span>
+                <span>1,200円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>あぶりまぐろセット</span>
+                <span>1,100円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ミックスフライセット(エビ・アジ・タイ)</span>
+                <span>1,200円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>エビフライセット</span>
+                <span>1,200円</span></p>
+                </td>
+                </tr>
+
+                </tbody>
+            </table>
+            </div>
+        </div>
+
+        <div class="table__box font-size--M">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_img03.jpg" alt="">
+            <div class="table__box-text">
+            <p class="table__box-title">寿司</p>
+            <table>
+                <tbody>
+                <tr>
+                <td>
+                <p class="flex"><span>上トロ寿司セット</span>
+                <span>1,850円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>中トロ寿司セット</span>
+                <span>1,550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ミックス寿司セット</span>
+                <span>1,550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろ赤身寿司(単品)</span>
+                <span>1,000円</span></p>
+                </td>
+                </tr>
+
+                </tbody>
+            </table>
+            </div>
+        </div>
+
+        <div class="table__box font-size--M">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_img04.jpg" alt="">
+            <div class="table__box-text">
+            <p class="table__box-title">お子様メニュー</p>
+            <table>
+                <tbody>
+                <tr>
+                <td>
+                <p class="flex"><span style="text-align: left;">お子様ランチ和風セット<br><span style="font-size: 10px;">寿司・マグロ丼・いくら丼・ネギトロ丼の<br>いずれかお一つお選びください。</span></span>
+                <span>各850円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>お子様ランチ洋風セット(エビフライ)</span>
+                <span>810円</span></p>
+                </td>
+                </tr>
+                </tbody>
+            </table>
+            <p style="margin: 20px auto 0; text-align:left; width: 95%;">※ドリンクは、コーラ・メロン・オレンジ・ジンジャーエール・ウーロン茶からお選びください。</p>
+            </div>
+        </div>
+
+        <div class="table__box font-size--M">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_img05.jpg" alt="">
+            <div class="table__box-text">
+            <p class="table__box-title">刺身・盛合</p>
+            <table>
+                <tbody>
+                <tr>
+                <td>
+                <p class="flex"><span>上トロの刺身</span>
+                <span>1,650円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>中トロの刺身</span>
+                <span>1,250円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>赤身の刺身</span>
+                <span>790円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>盛合1人前</span>
+                <span>1,250円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>盛合2・3人前</span>
+                <span>2,550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>盛合4・5人前</span>
+                <span>3,800円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>盛合6・7人前</span>
+                <span>5,100円</span></p>
+                </td>
+                </tr>
+                </tbody>
+            </table>
+            </div>
+        </div>
+
+        <div class="table__box font-size--M">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_img06.jpg" alt="">
+            <div class="table__box-text">
+            <p class="table__box-title">サラダ</p>
+            <table>
+                <tbody>
+
+                <tr>
+                <td>
+                <p class="flex"><span>加一まぐろのサラダ</span>
+                <span>950円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ツナサラダ</span>
+                <span>550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>トーフサラダ</span>
+                <span>550円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>野菜たっぷりサラダ</span>
+                <span>750円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろと10種類の野菜サラダ</span>
+                <span>800円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ミックスボールサラダ</span>
+                <span>800円</span></p>
+                </td>
+                </tr>
+
+                </tbody>
+            </table>
+            </div>
+        </div>
+
+        <div class="table__box font-size--M">
+
+            <div class="table__box-text">
+            <p class="table__box-title">揚げ物</p>
+            <table>
+
+                <tbody>
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろカツ</span>
+                <span>750円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>エビフライ単品</span>
+                <span>1,150円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>トロカツ(カジキマグロ)</span>
+                <span>1,000円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>アジフライ</span>
+                <span>650円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>串揚げ</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>大粒カキフライ※10〜3月限定</span>
+                <span>1,000円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ポテトフライ</span>
+                <span>380円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ポテトの特製ソース和え</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ポテトの天ぷら</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>揚げだし豆腐</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>鶏の唐揚げ</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>イカの唐揚げ</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろのすじの唐揚げ</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                </tbody>
+            </table>
+            </div>
+        </div>
+
+        <div class="table__box font-size--M">
+
+            <div class="table__box-text">
+            <p class="table__box-title">焼き物</p>
+            <table>
+                <tbody>
+
+                <tr>
+                <td>
+                <p class="flex"><span>あぶりまぐろ</span>
+                <span>750円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ホッケの開き</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ブリの塩焼き</span>
+                <span>650円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>イカ焼き</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>イカの生姜焼き</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>まぐろのパチパチ串焼き</span>
+                <span>500円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>ブリの和風ステーキ</span>
+                <span>650円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>カジキの和風ステーキ</span>
+                <span>650円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>サーモン和風ステーキ</span>
+                <span>650円</span></p>
+                </td>
+                </tr>
+
+                <tr>
+                <td>
+                <p class="flex"><span>あゆの塩焼き(2尾)※春夏限定</span>
+                <span>950円</span></p>
+                </td>
+                </tr>
+                </tbody>
+            </table>
+            </div>
+            </div>
+        </div>
+
+
+    </div>
+</section>
+
+<section id="topics" class="topics syokuji">
+    <h3><div class="title"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/syokuji/syokuji_title_02_text.png" alt="トピックス"></div></h3>
+    <div class="inner">
+        <div class="detail">
+            <ul class="list">
+            <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <li class="list__item">
+                            <dl>
+                                <dt class="text font-size--LL">
+                                    <h3><?php the_title(); ?></h3>
+                                </dt>
+                                <time datetime="<?php the_time('Y-m-d'); ?>" class="text font-size--S"><?php the_time('Y.m.d'); ?></time>
+                                <?php if(has_post_thumbnail()): ?>
+                                <?php the_post_thumbnail(); ?>
+                                <?php else: ?>
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage_l.png" alt="">
+                                <?php endif; ?>
+                                <dd class="text font-size--M"><?php the_content(); ?></dd>
+                            </dl>
+                        </li>
+                    <?php endwhile; ?>
+                <?php endif; ?>l>
+            </ul>
+        </div>
+        <div class="category">
+            <ul class="list flex">
+                <li class="list__item font-size--LL"><a href="<?php echo home_url(); ?>/suisan/">水産</a></li>
+                <li class="list__item font-size--LL"><a href="<?php echo home_url(); ?>/seika/">青果</a></li>
+                <li class="list__item font-size--LL"><a href="<?php echo home_url(); ?>/syokuniku">食肉</a></li> 
+            </ul>
+        </div>
+    </div>
+<?php get_footer(); ?>
---
+++ wp/wp-content/themes/kouryubussankan/archive-toretate.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+<div class="main-visual toretate">
+    <div class="container container--main-visual">
+        <h2><img src="<?php echo get_template_directory_uri(); ?>/assets/img/toretate/toretate-title.png" alt="お知らせ・イベント情報" class="main-visual__img"></h2>
+    </div>
+</div>
+
+
+<section class="topics toretate">
+    <div class="inner">
+        <div class="detail">
+            <?php if(have_posts()):
+                while(have_posts()): the_post(); ?>
+
+            <ul class="list">
+                <li class="list__item">
+                    <dl>
+                        <dt class="text font-size--LL"><h3><?php echo the_title(); ?></h3></dt>
+                        <time datetime="<?php the_time('Y-m-d'); ?>" class="text font-size--S"><?php the_time('Y.m.d'); ?></time>
+                        <?php if(has_post_thumbnail()): ?>
+                        <?php the_post_thumbnail(); ?>
+                        <?php else: ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage_l.png" alt="">
+                        <?php endif; ?>
+                        <dd class="text font-size--M"><?php the_content(); ?></dd>
+                    </dl>
+                </li>
+            </ul>
+            <?php endwhile;
+            endif; ?>
+        </div>
+        <div class="category">
+            <ul class="list flex">
+                <li class="list__item font-size--LL flex-item"><a href="<?php echo home_url(); ?>/suisan/">水産</a></li>
+                <li class="list__item font-size--LL"><a href="<?php echo home_url(); ?>/seika/">青果</a></li>
+                <li class="list__item font-size--LL"><a href="<?php echo home_url(); ?>/syokuniku/">食肉</a></li> 
+                <li class="list__item font-size--LL"><a href="<?php echo home_url(); ?>/syokuji/">お食事</a></li> 
+            </ul>
+        </div>
+    </div>
+<?php get_footer(); ?>
